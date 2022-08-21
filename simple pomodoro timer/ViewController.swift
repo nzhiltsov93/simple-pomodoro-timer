b78d715a4561f9692d9a10eaed9cd5dcf7f6fd77
NikitaZhiltsov <nzhiltsov93@gmail.com>
2022-08-21 22:15:39 +0300

    Initial Commit

---
+++ simple pomodoro timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple pomodoro timer
+//
+//  Created by Nikita Zhiltsov on 21.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
